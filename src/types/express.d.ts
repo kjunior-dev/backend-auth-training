d8df2c1df516e4f2f0a51a1175c08b69094e19ec
kjunior-dev <kevinjunior.dev@outlook.com>
2026-06-07 19:21:14 -0100

    commit "fix: generate prisma client before build"

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,15 +1,13 @@
 import type { Role } from "@prisma/client";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        id: string;
-        name: string;
-        email: string;
-        role: Role;
-      };
-    }
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: {
+      id: string;
+      name: string;
+      email: string;
+      role: Role;
+    };
   }
 }
 
